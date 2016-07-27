2d1b2c88f897f6acdd003e65ed74b175c0c02471
Andreas Argelius <andreas@argeli.us>
2016-07-27 12:43:48 +0900

    Make OnsNavigator into a mixin!

---
+++ src/Navigator.vue
@@ -0,0 +1,11 @@
+<script>
+  import {OnsNavigator} from './vue-onsenui';
+  import Page from './Page.vue';
+
+  export default {
+    mixins: [OnsNavigator],
+    components: {
+      Page
+    }
+  }
+</script>
